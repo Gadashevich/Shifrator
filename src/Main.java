67e5ba61a2b74015eeec1840a8f9e82e5bb62e75
GadashJohn <fox5b21@gmail.com>
2022-04-13 03:13:34 +0300

    # 10 commit: - Полностью переделан Шифр Цезаря, доработано меню

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
 public class Main {
 
+
+
     public static void main(String[] args) throws Exception {
-       Menu.mainMenu();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Menu menu = new Menu();
+        menu.mainMenu();
 
     }
 }
